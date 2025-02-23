2dd387f847b3045aa8b665e062b7d080e0b855c4
WXBO <xavilitoh@gmail.com>
2025-02-22 22:12:22 -0400

    Add Marca management features including creation, listing, and repository implementation
    Signed-off-by: WXBO <xavilitoh@gmail.com>

---
+++ EM/EM/Repositorio/MarcaRepositorio.cs
@@ -0,0 +1,111 @@
+using EM.Data;
+using EM.Entidades;
+using EM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EM.Repositorio;
+
+public interface IMarcaRepositorio : IRepositorioBase<Marca>
+{
+}
+
+public class MarcaRepositorio : IMarcaRepositorio
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public MarcaRepositorio(ApplicationDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    public async Task<Marca> Save(Marca modelo)
+    {
+        try
+        {
+            var result = await _dbContext.Marcas.AddAsync(modelo);
+            await _dbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error al guardar la Marca");
+        }
+    }
+
+    public async Task<int> Update(Marca modelo)
+    {
+        try
+        {
+            var result = _dbContext.Marcas.Update(modelo);
+            await _dbContext.SaveChangesAsync();
+            return result.Entity.Id;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error al actualizar la Marca");
+        }
+    }
+
+    public async Task<Marca?> Get(int id)
+    {
+        try
+        {
+            var result = await _dbContext
+                .Marcas
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.Id == id);
+            return result;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error al obtener la Marca");
+        }
+    }
+
+    public async Task<List<Marca>> Get()
+    {
+        try
+        {
+            var result = await _dbContext
+                .Marcas
+                .AsNoTracking()
+                .ToListAsync();
+            return result;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error al obtener las Marcas");
+        }
+    }
+
+    public async Task<List<SelectListItem>> SelectList()
+    {
+        try
+        {
+            var result = await _dbContext
+                .Marcas
+                .AsNoTracking()
+                .Select(a => new SelectListItem
+                {
+                    Id = a.Id,
+                    Value = a.Descripcion
+                })
+                .ToListAsync();
+            return result;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error al obtener las Marcas");
+        }
+    }
+
+    public async Task<bool> CambiaEstado(int id, bool nuevoEstado = false)
+    {
+        return false;
+    }
+}
