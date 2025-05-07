b9f32a3ead4cf5ce49d2fb9a7f25bb4119a084ba
Xavier Berroa <xavilitoh@gmail.com>
2025-05-06 20:06:31 -0400

    Add role seeding functionality and assign roles to default users during initialization

--- EM/EM/Program.cs
+++ EM/EM/Program.cs
@@ -100,9 +100,11 @@ using (var scope = app.Services.CreateScope())
     try
     {
         await context.Database.MigrateAsync();
+        await SeedDefaultRoles(services);
         await SeedDefaultUser(services);
         await SeedDefaultDisciplinas(services);
         await SeedDefaultMarcas(services);
+        await SeedDefaultRoles(services);
     }
     catch (Exception e)
     {
@@ -133,14 +135,16 @@ static async Task SeedDefaultUser(IServiceProvider serviceProvider)
     if (user == null)
     {
         user = new ApplicationUser
-        {
-            Nombre = "Usuario",
-            Apellido = "Prueba",
-            UserName = "usuario1@prueba.dev",
-            Email = "usuario1@prueba.dev",
-            EmailConfirmed = true
-        };
-        await userManager.CreateAsync(user, "Abcd1234.");
+            {
+                Nombre = "Usuario",
+                Apellido = "Prueba",
+                UserName = "usuario1@prueba.dev",
+                Email = "usuario1@prueba.dev",
+                EmailConfirmed = true
+            };
+            await userManager.CreateAsync(user, "Abcd1234.");
+            await userManager.AddToRoleAsync(user, "Admin");
+            await userManager.AddToRoleAsync(user, "Seguridad");
         
         ApplicationUser user2 = new ApplicationUser
         {
@@ -151,6 +155,22 @@ static async Task SeedDefaultUser(IServiceProvider serviceProvider)
             EmailConfirmed = true
         };
         await userManager.CreateAsync(user2, "Abcd1234.");
+        await userManager.AddToRoleAsync(user2, "Seguridad");
+    }
+}
+
+static async Task SeedDefaultRoles(IServiceProvider serviceProvider)
+{
+    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var roles = new[] { "Admin", "Seguridad", "Almacenes", "Usuario" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
     }
 }
 
